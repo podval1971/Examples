5f181e62f060e08e3f03201471c3b71eb31c07d2
Valery1971 <podval1971@gmail.com>
2023-03-09 20:13:02 +0300

    MethodArray

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,14 @@
+﻿int [] array = {51,26,13,54,75,26,71,8,19,12,34,67,8};
+int n = array.Length;
+int find = 8;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+    Console.WriteLine(index);
+    break;
+    }
+   index++; 
+}
